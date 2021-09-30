af9af388793ea065100fcda745a75bfd6f8b6613
qfunq <jonathan@fourcube.net>
2021-10-01 00:28:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-7ltawm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-7ltawm)
---
+++ worker.js
@@ -0,0 +1,14 @@
+// This is a module worker, so we can use imports (in the browser too!)
+function pi(n) {
+  var v = 0
+  for (let i = 1; i <= n; i += 4) {
+    // increment by 4
+    v += 1 / i - 1 / (i + 2) // add the value of the series
+  }
+  return 4 * v // apply the factor at last
+}
+
+
+addEventListener('message', (event) => {
+  postMessage(pi(event.data))
+})
